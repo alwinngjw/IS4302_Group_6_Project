bb7a60b504edeed73682fad3cab289989a67fa1a
Alwin Ng Jun Wei <alwinngjw@gmail.com>
2023-02-21 17:25:54 +0800

    removed additional comments

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,14 +1,6 @@
 const ERC20 = artifacts.require("ERC20");
 const USDCToken = artifacts.require("USDC");
 
-
-
-/*
-module.exports = (deployer , network, accounts) => {
-    deployer.deploy(Dice).then(() => deployer.deploy(DiceToken).then(() => deployer.deploy(DiceMarketDT, Dice.address, DiceToken.address, 1)));
-}
-*/
-
 module.exports = (deployer, network, accounts) => {
     deployer.deploy(USDCToken)
 };
