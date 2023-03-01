ccba7668a34ff8a9d9d6e81bedbce68ee63d1efc
hengyjj <hengyjj@hotmail.com>
2023-03-01 11:55:02 +0800

    updated test.js

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 const _deploy_contracts = require("../migrations/2_deploy_contracts");
-const truffleAssert = require("truffle-assertions");
+const truffleAssert = require("truffle-assertions"); //npm install truffle-assertions
 const BigNumber = require('bignumber.js'); // npm install bignumber.js
 var assert = require("assert");
 
