fc33200f9b7ba8f2cf358586268bc70e76a64357
Alwin Ng Jun Wei <alwinngjw@gmail.com>
2023-03-09 20:08:21 +0800

    updated test.js

--- test/test.js
+++ test/test.js
@@ -1,5 +1,3 @@
-<<<<<<< Updated upstream
-=======
 const _deploy_contracts = require("../migrations/2_deploy_contracts");
 const truffleAssert = require("truffle-assertions"); //npm install truffle-assertions
 const BigNumber = require('bignumber.js'); // npm install bignumber.js
@@ -165,4 +163,3 @@ contract('Liquidity Pool', function(accounts) {
         
     });
 });
->>>>>>> Stashed changes
