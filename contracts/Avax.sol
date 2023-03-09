c2496a3c5ddbcb4a3bc6eea16e2c7ce2ddf6b828
Alwin Ng Jun Wei <alwinngjw@gmail.com>
2023-03-09 17:57:05 +0800

    added Avax.sol and finshed liquidation mechanism , repay and borrow functions for both Avax and Eth

---
+++ contracts/Avax.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+import "./ERC20.sol";
+
+//Assume 1 ether = 2000 USD
+//1 ether should be 2000 USDC
+
+contract Avax is ERC20 {
+    uint256 supplyLimit;
+    uint256 currentSupply;
+    //address owner;
+
+    constructor() public {
+        owner = msg.sender;
+        supplyLimit = 1000000;
+    }
+
+    function getCredit() public payable {
+        uint256 amt = msg.value / 20000000000000000;
+        require(totalSupply() + amt < supplyLimit, "Token Supply is not enough");
+        mint(msg.sender, amt);
+    }
+    function checkCredit() public view returns (uint256) {
+        return balanceOf(msg.sender);
+    }
+}
