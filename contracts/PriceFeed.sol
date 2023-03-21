606c6b271e6a7f3ad480913a7509f768c4aef75c
Alwin Ng Jun Wei <alwinngjw@gmail.com>
2023-03-21 21:54:55 +0800

    added new test file to test the PriceFeed Oracle

--- contracts/PriceFeed.sol
+++ contracts/PriceFeed.sol
@@ -1,17 +1,17 @@
 pragma solidity ^0.8.7;
 
 contract PriceFeed {
-    function getEthPriceFirst() public view returns (uint256) {
+    function getEthPriceFirst() public pure returns (uint256) {
         return 1000;
     }
-    function getEthPriceToLiquidate() public view returns (uint256) {
+    function getEthPriceToLiquidate() public pure returns (uint256) {
         return 850; //Simulation purpose
     }
 
-    function getAvaxPriceFirst() public view returns (uint256) {
+    function getAvaxPriceFirst() public pure returns (uint256) {
         return 100;
     }
-    function getAvaxPriceToLiquidate() public view returns (uint256) {
+    function getAvaxPriceToLiquidate() public pure returns (uint256) {
         return 85;
     }
 }
