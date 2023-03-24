01d4f876506e8e7c53c03786911c3763741d7646
Alwin Ng Jun Wei <alwinngjw@gmail.com>
2023-03-24 12:23:14 +0800

    fixed test for borrowing ETH

--- test/test_Lending_BorrowETH.js
+++ test/test_Lending_BorrowETH.js
@@ -28,11 +28,28 @@ contract("Lending Contract (Borrow ETH function)", function (accounts) {
       );
     });
 
-    it("2. Testing whether User and Reserves receive correct amount of ETH respectively", async () => {
+    it("2. Testing Percentage calculation", async () => {
+      let fee = await lendingInstance.calculatePercentage(
+        BigNumber(0.95 * oneEth),
+        8000
+      ); 
+
+      fee = Number(fee / oneEth);
+
+      await assert.strictEqual(
+        fee,
+        0.76,
+        "The percentage calculation is Wrong!."
+      );
+    });
+
+    it("3. Testing whether User, Reserves and LP receives and deduct the correct amount of ETH respectively", async () => {
        let fee = await lendingInstance.calculatePercentage(BigNumber(0.95 * oneEth), 8000);
        fee = fee / oneEth
 
-       console.log(Number(fee));
+       //console.log(Number(fee));
+
+       await liquidityPoolInstance.transferEth({from : accounts[0], value : oneEth})
 
        //Test Reserves
        let reservesBalanceBefore = await web3.eth.getBalance(
@@ -40,25 +57,42 @@ contract("Lending Contract (Borrow ETH function)", function (accounts) {
        );
        reservesBalanceBefore = Number(reservesBalanceBefore / oneEth);
 
+       //Test LP
+       let LPBalanceBefore = await web3.eth.getBalance(
+         liquidityPoolInstance.address
+       );
+
+       LPBalanceBefore = Number(LPBalanceBefore / oneEth);
+       console.log("This is the LP Balance before " + LPBalanceBefore);
+
        let originalBalance = await web3.eth.getBalance(accounts[5]);
        originalBalance = originalBalance / oneEth;
        originalBalance = Number(originalBalance);
-       console.log("This is the Original Balance = " + originalBalance);
+       //console.log("This is the Original Balance = " + originalBalance);
   
-        await liquidityPoolInstance.transferEth({from: accounts[0], value: (oneEth)})
+        //await liquidityPoolInstance.transferEth({from: accounts[0], value: (oneEth)})
         await lendingInstance.borrowEth({ from: accounts[5], value: oneEth }); 
 
         let newBalance = await web3.eth.getBalance(accounts[5]);
         newBalance = Number(newBalance / oneEth);
-        console.log("This is the New Balance = " + newBalance);
+        //console.log("This is the New Balance = " + newBalance);
 
         let expectedBalance = ((originalBalance - (oneEth / oneEth)) + fee);
         expectedBalance = Number(expectedBalance);
-        console.log("This is the Expected Balance = " + (expectedBalance));
+        //console.log("This is the Expected Balance = " + (expectedBalance));
 
         reservesBalanceAfter = reservesBalanceBefore + 0.05;
         reservesBalanceAfter = Number(reservesBalanceAfter);
 
+        LPBalanceExpected = LPBalanceBefore - 0.76;
+        LPBalanceExpected = Number(LPBalanceExpected);
+
+        LPBalanceAfter = await web3.eth.getBalance(
+          liquidityPoolInstance.address
+        );
+        LPBalanceAfter = Number(LPBalanceAfter / oneEth);
+        
+       //console.log("This is the LP Balance after " + LPBalanceAfter);
           
         await assert.strictEqual(
           Math.floor(newBalance),
@@ -67,10 +101,17 @@ contract("Lending Contract (Borrow ETH function)", function (accounts) {
         );
 
         await assert.strictEqual(
-            Math.floor(reservesBalanceBefore),
-            Math.floor(reservesBalanceAfter),
-            "The percentage calculation is Wrong!."
-          );
+          Math.floor(reservesBalanceBefore),
+          Math.floor(reservesBalanceAfter),
+          "The percentage calculation is Wrong!."
+        );
+
+        
+        await assert.strictEqual(
+          LPBalanceExpected,
+          LPBalanceAfter,
+          "The percentage calculation is Wrong!."
+        );
       });
     
   /*
